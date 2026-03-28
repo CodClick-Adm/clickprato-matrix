98ed1bb2dced54aa7b9860c5e5a11c7ab87e64f8
CodClick-Adm <codclick.adm@gmail.com>
2026-03-28 16:30:56 -0300

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -5,9 +5,6 @@ import type { Database } from './types';
 const SUPABASE_URL = "__SUPABASE_URL__";
 const SUPABASE_PUBLISHABLE_KEY = "__SUPABASE_ANON__";
 
-// Import the supabase client like this:
-// import { supabase } from "@/integrations/supabase/client";
-
 export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY, {
   auth: {
     storage: localStorage,
