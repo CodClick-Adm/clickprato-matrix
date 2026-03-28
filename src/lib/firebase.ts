025c1a937c6c29d11bc0a195f4e401146795fd39
CodClick-Adm <codclick.adm@gmail.com>
2026-03-28 16:32:33 -0300

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,27 +1,16 @@
-
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
-
-// Configuração do Firebase
 const firebaseConfig = {
-"SEM CONFIGURACAO"
+"__FIREBASE_JSON__"
 };
-// Inicializar o Firebase
 const app = initializeApp(firebaseConfig);
-
-// Exportar o serviço de autenticação
 export const auth = getAuth(app);
-
-// Inicializar o Firestore
 export const db = getFirestore(app);
-
-// Inicializar Analytics somente no navegador para evitar erros em SSR
 let analytics = null;
 if (typeof window !== 'undefined') {
   analytics = getAnalytics(app);
 }
-
 export { analytics };
 export default app;
